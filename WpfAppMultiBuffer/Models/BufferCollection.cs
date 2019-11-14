da8574332fc5936ede39583ceb4f8b295640462b
Efanov P.A <efanov.pa.a@gmail.com>
2019-11-14 13:27:04 +0300

    Added new classes, in place of TwiceKeyDictionary. Made new classes simple and light.

---
+++ WpfAppMultiBuffer/Models/BufferCollection.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using WpfAppMultiBuffer.Extends;
+
+namespace WpfAppMultiBuffer.Models
+{
+    class BufferCollection : ObservableCollection<BufferItem>
+    {
+        /// <summary>
+        /// Добавляет новое содержимое в буфер
+        /// </summary>
+        /// <param name="refKey">Ссылочный ключ, предоставляет доступ к значимому ключу</param>
+        /// <param name="valueKey">Значимый ключ, предоставляет доступ к значению</param>
+        /// <param name="value">Значение</param>
+        public void Add(Keys refKey, Keys valueKey, string value)
+        {
+            if (refKey == valueKey)
+                throw new Exception("Ref key and value key must be unique.");
+
+            BufferItem item = new BufferItem
+            {
+                Index = base.Count,
+                RefKey = refKey,
+                ValueKey = valueKey,
+                Value = value,
+            };
+
+            base.Add(item);
+        }
+        /// <summary>
+        /// Добавляет массив элементов в буфер
+        /// </summary>
+        /// <param name="refKeys">Массив ссылоных ключей</param>
+        /// <param name="valueKeys">Массив значимых ключей, длина должна быть равна массиву ссылочных ключей</param>
+        /// <param name="value">Значение по умолчанию</param>
+        public void AddRange(Keys[] refKeys, Keys[] valueKeys, string value)
+        {
+            if (refKeys.Length != valueKeys.Length)
+                throw new Exception("Count key must be same.");
+
+            for (int i = 0; i < refKeys.Length; i++)
+            {
+                Add(refKeys[i], valueKeys[i], value);
+            }
+        }
+        /// <summary>
+        /// Получить или установить значение буфера по клавише
+        /// </summary>
+        /// <param name="inputKey">Нажатая клавиша</param>
+        /// <returns>Возвращает значение буфера</returns>
+        public string this[Keys inputKey]
+        {
+            get
+            {
+                string value;
+
+                try
+                {
+                    value =
+                        (from el in this
+                         where el.RefKey == inputKey || el.ValueKey == inputKey
+                         select el.Value).Single();
+                }
+                catch (ArgumentNullException e)
+                {
+                    throw new ArgumentNullException("Buffer collection is null", e);
+                }
+                catch (InvalidOperationException e)
+                {
+                    throw new InvalidOperationException("Did not find input key in buffer collection", e);
+                }
+
+                return value;
+            }
+            set
+            {
+                BufferItem item =
+                    (from el in this
+                    where el.RefKey == inputKey || el.ValueKey == inputKey
+                    select el).Single();
+
+                try
+                {
+                    item =
+                        (from el in this
+                         where el.RefKey == inputKey || el.ValueKey == inputKey
+                         select el).Single();
+                }
+                catch (ArgumentNullException e)
+                {
+                    throw new ArgumentNullException("Buffer collection is null", e);
+                }
+                catch (InvalidOperationException e)
+                {
+                    throw new InvalidOperationException("Did not find input key in buffer collection", e);
+                }
+
+                item.Value = value;
+                base.SetItem(item.Index, item);
+            }
+        }
+    }
+}
---
+++ WpfAppMultiBuffer/Models/BufferItem.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfAppMultiBuffer.Extends;
+
+namespace WpfAppMultiBuffer.Models
+{
+    class BufferItem : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Индекс элемента в коллецкии, заполняется автоматический при добавлении в BufferCollection
+        /// </summary>
+        public int Index { get; set; }
+        /// <summary>
+        /// Ссылочный ключ, предоставляет доступ к значимому ключу
+        /// </summary>
+        public Keys RefKey { get; set; }
+        /// <summary>
+        /// Значимый ключ, предоставляет доступ к значению
+        /// </summary>
+        public Keys ValueKey { get; set; }
+        /// <summary>
+        /// Хранимое значение
+        /// </summary>
+        string _value;
+        /// <summary>
+        /// Значение.
+        /// Оповещает о своем изменении
+        /// </summary>
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+        /// <summary>
+        /// Свойство обновлено
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Функция вызова события "Свойство обновленно"
+        /// </summary>
+        /// <param name="propertyName">Имя вызывающего метода или свойства. Заполняется автоматически</param>
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
