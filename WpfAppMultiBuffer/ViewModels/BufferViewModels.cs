015808f9260dff7ecabb1602cce57306fc2ba7d2
Efanov P.A <efanov.pa.a@gmail.com>
2019-11-21 16:24:04 +0300

    Replace win forms timer on DispatcherTimer.

--- WpfAppMultiBuffer/ViewModels/BufferViewModels.cs
+++ WpfAppMultiBuffer/ViewModels/BufferViewModels.cs
@@ -1,11 +1,11 @@
 ﻿using System.Linq;
-using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
 using TextCopy;
 using WpfAppMultiBuffer.Models;
 using WpfAppMultiBuffer.Views;
 using System;
+using System.Windows.Threading;
 
 namespace WpfAppMultiBuffer.ViewModels
 {
@@ -37,15 +37,18 @@ namespace WpfAppMultiBuffer.ViewModels
                 inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_C);
                 inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LCONTROL);
 
-                Timer timer = new Timer();//TODO: Сделать нормальный асинхорн, убрать таймер
+                DispatcherTimer timer = new DispatcherTimer()
+                { 
+                    Interval = TimeSpan.FromMilliseconds(Literals.Interval), 
+                };
+
                 timer.Tick += (timerInner, eventArgs) =>
                 {
                     timer.Stop();
                     Storage[key.InputKey] = TextCopy.Clipboard.GetText();
                     TextCopy.Clipboard.SetText(contentsClipboard);
-                    timer.Dispose();
                 };
-                timer.Interval = Literals.Interval;
+
                 timer.Start();
             }
             else if (Literals.KeysPaste.Contains(key.InputKey) && Storage[key.InputKey] != null && Storage[key.InputKey] != "")
@@ -56,14 +59,16 @@ namespace WpfAppMultiBuffer.ViewModels
                 inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_V);
                 inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LCONTROL);
 
-                Timer timer = new Timer();//TODO: Сделать нормальный асинхорн, убрать таймер
+                DispatcherTimer timer = new DispatcherTimer()
+                {
+                    Interval = TimeSpan.FromMilliseconds(Literals.Interval),
+                };
+
                 timer.Tick += (timerInner, eventArgs) =>
                 {
                     timer.Stop();
                     TextCopy.Clipboard.SetText(contentsClipboard);
-                    timer.Dispose();
                 };
-                timer.Interval = Literals.Interval;
                 timer.Start();
             }
         }
