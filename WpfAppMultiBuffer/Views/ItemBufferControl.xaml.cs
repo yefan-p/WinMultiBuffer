46075d01144315ecd5ecef282db60efd7c305448
Efanov P.A <efanov.pa.a@gmail.com>
2019-11-29 12:53:08 +0300

    Revert commits with aniamtions

--- WpfAppMultiBuffer/Views/ItemBufferControl.xaml.cs
+++ WpfAppMultiBuffer/Views/ItemBufferControl.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Animation;
 
 namespace WpfAppMultiBuffer.Views
 {
@@ -16,65 +15,6 @@ namespace WpfAppMultiBuffer.Views
             Width = 0;
             Height = 0;
             Clear.Click += Clear_Click;
-            //SizeChanged += ItemBufferControl_SizeChanged;
-        }
-        bool animationWidthPlay = false;
-        bool animationHeightPlay = false;
-        /// <summary>
-        /// Отображает анимацию разворачивания и скрытия блока
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ItemBufferControl_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (!animationWidthPlay && !animationHeightPlay)
-            {
-                if (e.HeightChanged && e.PreviousSize.Height < e.NewSize.Height)
-                {
-                    e.Handled = true;
-                    animationHeightPlay = true;
-                    HideAnimation(e.PreviousSize.Height, e.NewSize.Height, HeightProperty, ref animationHeightPlay);
-                }
-                else if(e.HeightChanged && e.PreviousSize.Height > e.NewSize.Height)
-                {
-                    e.Handled = true;
-                    animationHeightPlay = true;
-                    HideAnimation(e.NewSize.Height, e.PreviousSize.Height, HeightProperty, ref animationHeightPlay);
-                }
-
-                if (e.WidthChanged && e.PreviousSize.Width < e.NewSize.Width)
-                {
-                    //e.Handled = true;
-                    animationWidthPlay = true;
-                    HideAnimation(e.PreviousSize.Width, e.NewSize.Width, WidthProperty, ref animationWidthPlay);
-                }
-                else if(e.WidthChanged && e.PreviousSize.Width > e.NewSize.Width)
-                {
-                    e.Handled = true;
-                    animationWidthPlay = true;
-                    HideAnimation(e.NewSize.Width, e.PreviousSize.Width, WidthProperty, ref animationWidthPlay);
-                }
-            }
-        }
-        /// <summary>
-        /// Воспроизвести анимацию для заданного свойства
-        /// </summary>
-        /// <param name="from">Начальное значение</param>
-        /// <param name="to">Конечное значение</param>
-        /// <param name="property">Свойство, которое будет меняться</param>
-        /// <param name="playFlag">Флаг, указывающий, что анимация воспроизводиться</param>
-        void HideAnimation(double from, double to, DependencyProperty property, ref bool playFlag)
-        {
-            bool flag = playFlag;
-            DoubleAnimation animation = new DoubleAnimation
-            {
-                From = from,
-                To = to,
-                Duration = new Duration(TimeSpan.FromMilliseconds(500)),
-            };
-            animation.Completed += (o, e) => { flag = false; };
-            playFlag = flag;
-            BeginAnimation(property, animation);
         }
         /// <summary>
         /// Клик по кнопке Очистить содержимое
