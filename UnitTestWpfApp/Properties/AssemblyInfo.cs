6426872be1af22dbad6b01463d44d253410cba25
Efanov P.A <efanov.pa.a@gmail.com>
2019-11-29 16:18:44 +0300

    Add test net fraemwork project for wpf application.

---
+++ UnitTestWpfApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWpfApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ЗАО "Техносерв Консалтинг"")]
+[assembly: AssemblyProduct("UnitTestWpfApp")]
+[assembly: AssemblyCopyright("Copyright © ЗАО "Техносерв Консалтинг" 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75007521-8912-436a-bb0a-112c51a96cf4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
