fa21fba484847c273f7677b2cc2e51922042b774
Efanov P.A <efanov.pa.a@gmail.com>
2019-12-13 12:41:33 +0300

    Delete interface implementation INotifyPropertyChanged

--- WpfAppMultiBuffer/ViewModels/BaseViewModel.cs
+++ WpfAppMultiBuffer/ViewModels/BaseViewModel.cs
@@ -1,28 +1,13 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using WpfAppMultiBuffer.Utils;
+﻿using WpfAppMultiBuffer.Utils;
 
 namespace WpfAppMultiBuffer.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel
     {
         protected INavigationManager NavigationManager { get; private set; }
         public BaseViewModel(INavigationManager navigationManager)
         {
             NavigationManager = navigationManager;
-        }
-
-        /// <summary>
-        /// Свойство обновлено. Возникает после обновления свойства
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-        /// <summary>
-        /// Функция вызова события "Свойство обновленно"
-        /// </summary>
-        /// <param name="propertyName">Имя вызывающего метода или свойства. Заполняется автоматически</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
