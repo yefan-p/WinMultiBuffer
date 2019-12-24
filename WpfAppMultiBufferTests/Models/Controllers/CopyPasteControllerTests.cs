030c53528613a5983e8d5e79b41010347aa89de1
Efanov P.A <efanov.pa.a@gmail.com>
2019-12-24 17:55:59 +0300

    Add unit test project for wpfApp

---
+++ WpfAppMultiBufferTests/Models/Controllers/CopyPasteControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfAppMultiBuffer.Models.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppMultiBuffer.Models.Controllers.Tests
+{
+    [TestClass()]
+    public class CopyPasteControllerTests
+    {
+        [TestMethod()]
+        public void CopyPasteControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PasteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
