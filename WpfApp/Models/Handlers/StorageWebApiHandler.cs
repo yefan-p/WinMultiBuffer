394c8282039a7a24326e43bfa41b63065825d0df
Efanov P.A <efanov.pa.a@gmail.com>
2020-03-09 19:24:05 +0300

    Added storageWebApi handler.

---
+++ WpfApp/Models/Handlers/StorageWebApiHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiBuffer.WpfApp.Models.Interfaces;
+
+namespace MultiBuffer.WpfApp.Models.Handlers
+{
+    public class StorageWebApiHandler
+    {
+        string _webApiAddress = "https://localhost:44315/api/Buffers/";
+
+        public void Create(IBufferItem item)
+        {
+
+        }
+    }
+}
