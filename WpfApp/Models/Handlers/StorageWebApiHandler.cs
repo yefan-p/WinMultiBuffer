3989e4ccac27eb8a7a2c0b335923b1c2592e548e
Efanov P.A <efanov.pa.a@gmail.com>
2020-03-12 13:37:09 +0300

    Added httpClient for webApi

--- WpfApp/Models/Handlers/StorageWebApiHandler.cs
+++ WpfApp/Models/Handlers/StorageWebApiHandler.cs
@@ -5,16 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using MultiBuffer.WpfApp.Models.Interfaces;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace MultiBuffer.WpfApp.Models.Handlers
 {
     public class StorageWebApiHandler
     {
-        string _webApiAddress = "https://localhost:44315/api/Buffers/";
-
-        public void Create(IBufferItem item)
+        public StorageWebApiHandler()
         {
-            var request = WebRequest.Create(_webApiAddress + (int)item.Key) as HttpWebRequest;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:44315/");
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
+
+        /// <summary>
+        /// Отправить новый элемент
+        /// </summary>
+        /// <param name="item"></param>
+        public async void CreateAsync(IBufferItem item)
+        {
+            HttpResponseMessage httpResponse = await _httpClient.PostAsJsonAsync(_webApiBuffers, item);
+            httpResponse.EnsureSuccessStatusCode();
+        }
+
+        /// <summary>
+        /// Клиент для отправки запросов
+        /// </summary>
+        HttpClient _httpClient;
+
+        /// <summary>
+        /// Адрес для работы с буферами
+        /// </summary>
+        string _webApiBuffers = "api/Buffers/";
     }
 }
