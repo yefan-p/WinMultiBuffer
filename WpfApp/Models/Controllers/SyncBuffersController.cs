2cadf5302e06069f637f19f4228cbf74d3e9a665
Efanov P.A <efanov.pa.a@gmail.com>
2020-03-20 20:13:57 +0300

    Added sync of buffers when HelpView start.

--- WpfApp/Models/Controllers/SyncBuffersController.cs
+++ WpfApp/Models/Controllers/SyncBuffersController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MultiBuffer.WpfApp.Models.Interfaces;
 using MultiBuffer.IWebApi;
-using System.Diagnostics;
 
 namespace MultiBuffer.WpfApp.Models.Controllers
 {
@@ -14,20 +14,51 @@ namespace MultiBuffer.WpfApp.Models.Controllers
     /// </summary>
     public class SyncBuffersController : ISyncBuffersController
     {
-        public SyncBuffersController(IWebApiHandler webApi)
+        public SyncBuffersController(IWebApiHandler webApi,
+                                     IList<IBufferItem> buffers,
+                                     IBufferItemFactory bufferItemFactory)
         {
-            RunSynchronization(webApi);
-        }
+            _buffers = buffers;
+            _webApiHandler = webApi;
 
-        void RunSynchronization(IWebApiHandler webApi)
-        {
             Task task = new Task(async () =>
             {
                 await webApi.AuthUser("admin", "admin"); //TODO: брать из настроек
                 IEnumerable<WebBuffer> webBuffers = await webApi.ReadListAsync();
+
+                foreach (WebBuffer item in webBuffers)
+                {
+                    IBufferItem bufferItem = bufferItemFactory.GetBuffer();
+                    bufferItem.Key = (Keys)item.Key;
+                    bufferItem.Value = item.Value;
+                    bufferItem.Delete += BufferItem_Delete;
+                    App.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+                        _buffers.Add(bufferItem);
+                    }));
+                }
             });
             task.Start();
         }
 
+        /// <summary>
+        /// Обращение к api
+        /// </summary>
+        IWebApiHandler _webApiHandler;
+
+        /// <summary>
+        /// Коллекция буферов
+        /// </summary>
+        IList<IBufferItem> _buffers;
+
+        /// <summary>
+        /// Удаляет выбранный буфер
+        /// </summary>
+        /// <param name="obj"></param>
+        void BufferItem_Delete(IBufferItem obj)
+        {
+            _buffers.Remove(obj);
+            _webApiHandler.DeleteAsync((int)obj.Key);
+        }
     }
 }
