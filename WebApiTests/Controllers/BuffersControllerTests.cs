535041f470004fc15d2b1a763de7bc589fd9e67b
Efanov P.A <efanov.pa.a@gmail.com>
2020-03-24 19:39:56 +0300

    Was adding web api tests.

---
+++ WebApiTests/Controllers/BuffersControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiBuffer.WebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MultiBuffer.WebApi.Utils;
+using Microsoft.Extensions.Options;
+
+namespace MultiBuffer.WebApi.Controllers.Tests
+{
+    [TestClass()]
+    public class BuffersControllerTests
+    {
+        [TestMethod()]
+        public void CreateListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
